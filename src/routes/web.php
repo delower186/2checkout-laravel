df7f8537bdca448a8b22693b016e36ccf0992044
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-04 21:13:04 +0800

    Delete src directory

--- src/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::group(['namespace' => 'Delower186\Twocheckout\Http\Controllers'],function () {
-    
-    Route::get('twocheckout','TwocheckoutController@index');
-
-    Route::get('twocheckout/thankyou','TwocheckoutController@thankYou');
-
-});
